e33ed9422c860df6b1773594ad51b9691c78094d
Adasat Torres de León <a.torres@binhex.cloud>
2024-10-18 15:27:47 +0100

    [FIX]

--- survey_question_type_binary/static/src/js/survey_form.js
+++ survey_question_type_binary/static/src/js/survey_form.js
@@ -7,78 +7,56 @@ odoo.define("survey_question_type_binary", function (require) {
     const survey_form = require("survey.form");
 
     survey_form.include({
-        _getSubmitBinariesValues: function (params) {
-            const self = this;
-            let promises = [];
-
+        _prepareSubmitValues: function (formData, params) {
+            this._super(formData, params);
+            var self = this;
             this.$("[data-question-type]").each(function () {
                 switch ($(this).data("questionType")) {
                     case "binary":
                     case "multi_binary":
-                        promises = promises.concat(
-                            self._getSubmitAnswersBinary(params, $(this))
+                        params = self._prepareSubmitBinaries(
+                            params,
+                            $(this),
+                            $(this).data("name")
                         );
                         break;
                 }
             });
-            return promises;
         },
-        _getSubmitAnswersBinary: function (params, $input) {
-            const question_id = $input.attr("name");
-            return Array.prototype.map.call($input[0].files, (file) => {
-                return this._readFileAsDataURL(file).then(function (sDataURL) {
-                    if (!params[question_id]) {
-                        params[question_id] = [];
-                    }
-                    params[question_id].push({
-                        data: sDataURL.split(",")[1],
-                        filename: file.name,
-                        size: file.size,
-                        type: file.type,
-                    });
+
+        _prepareSubmitBinaries: function (params, $binaryField, questionId) {
+            console.log("params", params);
+            console.log("questionId", questionId);
+            console.log("$binaryField", $binaryField);
+            console.log("files", $binaryField[0].files);
+            const self = this;
+            params[questionId] = this._prepareBinaryDatas(
+                Object.values($binaryField[0].files)
+            );
+            return params;
+        },
+
+        _prepareBinaryDatas: function (files) {
+            const array = [];
+            files.each((file) => {
+                array.push({
+                    name: file.name,
+                    type: file.type,
+                    size: file.size,
+                    data: this._readFileAsDataURL(file),
                 });
             });
+            return array;
         },
-        _readFileAsDataURL: function (file) {
-            return $.Deferred(function (deferred) {
-                $.extend(new FileReader(), {
-                    onload: function (e) {
-                        var sDataURL = e.target.result;
-                        deferred.resolve(sDataURL);
-                    },
-                    onerror: function () {
-                        deferred.reject(this);
-                    },
-                }).readAsDataURL(file);
-            }).promise();
-        },
-        _submitForm: function (options) {
-            const self = this;
-            const params = {};
-            const binaryPrimises = this._getSubmitBinariesValues(params);
-            if (binaryPrimises.length > 0 && !this.options.isStartScreen) {
-                const $form = this.$("form");
-                const formData = new FormData($form[0]);
 
-                if (options.previousPageId) {
-                    params.previous_page_id = options.previousPageId;
-                }
-                this._prepareSubmitValues(formData, params);
-                Promise.all(binaryPrimises).then(function () {
-                    const submitPromise = self._rpc({
-                        route: _.str.sprintf(
-                            "%s/%s/%s",
-                            "/survey/submit",
-                            self.options.surveyToken,
-                            self.options.answerToken
-                        ),
-                        params: params,
-                    });
-                    self._nextScreen(submitPromise, options);
-                });
-            } else {
-                return this._super(options);
-            }
+        _readFileAsDataURL: async function (file) {
+            await new Promise((resolve) => {
+                const reader = new FileReader();
+                reader.onload = (e) => {
+                    resolve(e.target.result);
+                };
+                reader.readAsDataURL(file);
+            });
         },
     });
 });
