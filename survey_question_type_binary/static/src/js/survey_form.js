2003953f5868ba697ecf23756a21010ef644097e
Adasat Torres de León <adasat.torres.dev@gmail.com>
2024-10-19 14:33:05 +0100

    problemas con el async

--- survey_question_type_binary/static/src/js/survey_form.js
+++ survey_question_type_binary/static/src/js/survey_form.js
@@ -7,50 +7,55 @@ odoo.define("survey_question_type_binary", function (require) {
     const survey_form = require("survey.form");
 
     survey_form.include({
-        _prepareSubmitValues: function (formData, params) {
+        _prepareSubmitValues: async function (formData, params) {
             this._super(formData, params);
             var self = this;
+            const promises = [];
             this.$("[data-question-type]").each(function () {
                 switch ($(this).data("questionType")) {
                     case "binary":
                     case "multi_binary":
-                        params = self._prepareSubmitBinaries(
-                            params,
-                            $(this),
-                            $(this).data("name")
+                        promises.push(
+                            self._prepareSubmitBinaries(
+                                params,
+                                $(this),
+                                $(this).data("name")
+                            )
                         );
                         break;
                 }
             });
+            await Promise.all(promises);
         },
 
         _prepareSubmitBinaries: function (params, $binaryField, questionId) {
-            console.log("params", params);
-            console.log("questionId", questionId);
-            console.log("$binaryField", $binaryField);
-            console.log("files", $binaryField[0].files);
-            const self = this;
-            params[questionId] = this._prepareBinaryDatas(
-                Object.values($binaryField[0].files)
+            console.log("Binary2");
+            return this._prepareBinaryDatas(Object.values($binaryField[0].files)).then(
+                (binaryDatas) => {
+                    params[questionId] = binaryDatas;
+                }
             );
-            return params;
         },
 
-        _prepareBinaryDatas: function (files) {
+        _prepareBinaryDatas: async function (files) {
             const array = [];
-            files.each((file) => {
+
+            console.log("Binary3");
+            for (const file of files) {
+                const dataURL = await this._readFileAsDataURL(file);
                 array.push({
                     name: file.name,
                     type: file.type,
                     size: file.size,
-                    data: this._readFileAsDataURL(file),
+                    data: dataURL.split(",")[1],
                 });
-            });
+            }
+            console.log(array);
             return array;
         },
 
-        _readFileAsDataURL: async function (file) {
-            await new Promise((resolve) => {
+        _readFileAsDataURL: function (file) {
+            return new Promise((resolve) => {
                 const reader = new FileReader();
                 reader.onload = (e) => {
                     resolve(e.target.result);
